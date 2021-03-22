10ee11733603753c7f8e9874f86724adb4f023e6
Laurens <laurensmeijerink@gmail.com>
2021-03-22 22:45:16 +0100

    TestCommit 1

---
+++ Snake Oil Discord Bot/FileReader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Snake_Oil_Discord_Bot
+{
+    class FileReader
+    {
+        static Dictionary<string, string> GetSettings()
+        {
+            Dictionary<string, string> settings = new Dictionary<string, string>();
+
+            string line;
+            StreamReader file = new StreamReader("\\TextFiles\\Settings.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] pair = line.Split('=');
+                settings.Add(pair[0], pair[1]);
+            }
+            return settings;
+        }
+    }
+}
--- Snake Oil Discord Bot/Program.cs
+++ Snake Oil Discord Bot/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace Snake_Oil_Discord_Bot
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            string token = "";
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            _client.MessageReceived += readMessages;
+
+            // Block this task until the program is closed.
+            bool running = true;
+            while (running)
+            {
+                string s = Console.ReadLine();
+                if (s == "close")
+                {
+                    running = false;
+                    await _client.LogoutAsync();
+                }
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task readMessages(SocketMessage message)
+        {
+            Console.WriteLine(message.Content);
+            if (message.Content.Contains("boo"))
+                message.Channel.SendMessageAsync("whaaa", false);
+            return Task.CompletedTask;
         }
     }
 }
